c61da039198a278d7c87781399af2ddfb9c0c0c4
Leonardo Castro <castro.delg.leonardo@gmail.com>
2023-10-06 19:27:49 -0600

    LSCD Practica cinepolis 6/octubre/2023

--- src/app/calculos/cinepolis/cinepolis.component.ts
+++ src/app/calculos/cinepolis/cinepolis.component.ts
@@ -13,10 +13,16 @@ export class CinepolisComponent {
   boletosTotales:number=0;
   tarjetaCineco:string='';
   totalFinal:number=0;
+  subtotal:number=0;
+  descuento:number=0;
+  descuentoTarj:number=0;
   mensaje:string='';
 
   procesar(){
     this.totalFinal=0;
+    this.descuento=0;
+    this.subtotal=0;
+    this.descuentoTarj=0;
     this.boletosTotales=this.cantBoletos/parseInt(this.cantComprad);
     if(this.boletosTotales>7){
       this.mensaje='¡Solo se pueden comprar 7 boletos por persona como máximo!';
@@ -24,16 +30,56 @@ export class CinepolisComponent {
     else{
       if(this.boletosTotales<=2){
         if(this.tarjetaCineco=='si'){
-          this.totalFinal=(this.boletosTotales*12)*0.9;
+          this.descuentoTarj=(this.cantBoletos*12)*0.1;
+          this.totalFinal=(this.cantBoletos*12)-this.descuentoTarj;
+          this.mensaje='Nombre: '+ this.nombre +'\n Boletos totales: ' + this.cantBoletos +'\n Número de clientes: '+this.cantComprad;
         }
-        else{
-          this.totalFinal=this.boletosTotales*12;
+        else if(this.tarjetaCineco=='no'){
+          this.totalFinal=this.cantBoletos*12;
+          this.mensaje='Nombre: '+ this.nombre +'\n Boletos totales: ' + this.cantBoletos +'\n Número de clientes: '+this.cantComprad;
+        }
+      }
+      if(this.cantBoletos>2 && this.cantBoletos<=5){
+        if(this.tarjetaCineco=='si'){
+          this.descuento=(this.cantBoletos*12)*0.1;
+          this.subtotal=(this.cantBoletos*12)-this.descuento;
+          this.descuentoTarj=this.subtotal*0.1;
+          this.totalFinal=this.subtotal-this.descuentoTarj;
+          this.mensaje='Nombre: '+ this.nombre +'\n Boletos totales: ' + this.cantBoletos +'\n Número de clientes: '+this.cantComprad;
+        }
+        else if(this.tarjetaCineco=='no'){
+          this.descuento=(this.cantBoletos*12)*0.1;
+          this.totalFinal=(this.cantBoletos*12)-this.descuento;
+          this.mensaje='Nombre: '+ this.nombre +'\n Boletos totales: ' + this.cantBoletos +'\n Número de clientes: '+this.cantComprad;
+        }
+      }
+      if(this.cantBoletos>5){
+        if(this.tarjetaCineco=='si'){
+          this.descuento=(this.cantBoletos*12)*0.15;
+          this.subtotal=(this.cantBoletos*12)-this.descuento;
+          this.descuentoTarj=this.subtotal*0.1;
+          this.totalFinal=this.subtotal-this.descuentoTarj;
+          this.mensaje='Nombre: '+ this.nombre +'\n Boletos totales: ' + this.cantBoletos +'\n Número de clientes: '+this.cantComprad;
+        }
+        else if(this.tarjetaCineco=='no'){
+          this.descuento=(this.cantBoletos*12)*0.15;
+          this.totalFinal=(this.cantBoletos*12)-this.descuento;
+          this.mensaje='Nombre: '+ this.nombre +'\n Boletos totales: ' + this.cantBoletos +'\n Número de clientes: '+this.cantComprad;
         }
       }
     }
   }
 
   salir(){
-
+    this.nombre='';
+    this.cantComprad='';
+    this.cantBoletos=0;
+    this.boletosTotales=0;
+    this.tarjetaCineco='';
+    this.totalFinal=0;
+    this.mensaje='';
+    this.descuento=0;
+    this.subtotal=0;
+    this.descuentoTarj=0;
   }
 }
